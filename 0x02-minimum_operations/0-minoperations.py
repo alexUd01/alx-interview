d4007a5cccbb5ed9034cbcc69a9fe98561028821
alexUd01 <alexanderikpeama@gmail.com>
2023-07-11 12:29:40 +0100

    Added module documentaion to `0-minoperations.py`.

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,5 +1,23 @@
 #!/usr/bin/python3
-""" Don't forget to document later
+""" Interview Preparation Question:
+
+QUESTION:
+In a text file, there is a single character `H`. Your text editor can execute
+only two operations in this file: `Copy All` and `Paste`. Given a number `n`,
+write a method that calculates the fewest number of operations needed to
+result in exactly n `H` characters in the file.
+
+INSTRUCTION:
+Prototype: def minOperations(n)
+Returns an integer
+If n is impossible to achieve, return 0
+
+EXAMPLE:
+n = 9
+H => Copy All => Paste => HH => Paste =>HHH => Copy All => Paste => HHHHHH =>
+    Paste => HHHHHHHHH
+
+Number of operations: 6
 """
 
 
