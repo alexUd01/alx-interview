29bd81fe8e7907a1c0c48748ac5e3c691c2386bd
alexUd01 <alexanderikpeama@gmail.com>
2023-07-11 12:31:59 +0100

    Added module documentaion to `0-minoperations.py`.

--- 0x02-minimum_operations/0-main.py
+++ 0x02-minimum_operations/0-main.py
@@ -11,5 +11,5 @@ print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
 n = 12
 print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
 
-n = 100
+n = 9
 print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
