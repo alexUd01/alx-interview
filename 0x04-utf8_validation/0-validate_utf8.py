1f308be1cd814cbd7b63e1946d205767f1b2da52
alexUd01 <alexanderikpeama@gmail.com>
2023-07-25 13:55:22 +0100

    test this code: on the checker

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -14,72 +14,47 @@ INSTRUCTIONS:
 
 
 def validUTF8(data):
-    """
-    Check if the given data represents a valid UTF-8 encoding.
+    """ The function """
+    number_bytes = 0
 
-    Args:
-    data (list[int]): A list of integers representing 1-byte data.
+    mask_1 = 1 << 7
+    mask_2 = 1 << 6
 
-    Returns:
-    bool: True if data is a valid UTF-8 encoding, False otherwise.
-    """
+    for i in data:
 
-    if not isinstance(data, list):
-        return False
+        mask_byte = 1 << 7
 
-    def is_valid_code_point(code_point):
-        # Check if the code point is within valid Unicode range
-        if (0xD800 <= code_point <= 0xDFFF) or (0xFDD0 <= code_point <= 0xFDEF):
-            return False
-        return code_point <= 0x10FFFF
+        if number_bytes == 0:
 
-    def is_overlong_encoding(byte_count, code_point):
-        # Check for overlong encodings (UTF-8 characters represented with more bytes than necessary)
-        return (
-            (byte_count == 1 and code_point <= 0x7F)
-            or (byte_count == 2 and code_point <= 0x7FF)
-            or (byte_count == 3 and code_point <= 0xFFFF)
-            or (byte_count == 4 and code_point <= 0x10FFFF)
-        )
+            while mask_byte & i:
+                number_bytes += 1
+                mask_byte = mask_byte >> 1
 
-    idx = 0
-    while idx < len(data):
-        first_byte = data[idx]
+            if number_bytes == 0:
+                continue
 
-        if first_byte < 0x80:  # 1 byte encoding (regular ASCII characters)
-            idx += 1
-            continue
+            if number_bytes == 1 or number_bytes > 4:
+                return False
 
-        # Find the number of bytes in the UTF-8 character
-        if first_byte & 0xE0 == 0xC0:  # 2 bytes
-            byte_count = 2
-            code_point = first_byte & 0x1F
-        elif first_byte & 0xF0 == 0xE0:  # 3 bytes
-            byte_count = 3
-            code_point = first_byte & 0x0F
-        elif first_byte & 0xF8 == 0xF0:  # 4 bytes
-            byte_count = 4
-            code_point = first_byte & 0x07
         else:
-            # Invalid UTF-8 start byte
-            return False
+            if not (i & mask_1 and not (i & mask_2)):
+                    return False
 
-        idx += 1
+        number_bytes -= 1
 
-        # Check continuation bytes
-        for i in range(byte_count - 1):
-            if idx >= len(data):
-                return False
+    if number_bytes == 0:
+        return True
 
-            cont_byte = data[idx]
-            if cont_byte & 0xC0 != 0x80:  # Continuation byte must start with '10'
-                return False
+    return False
 
-            code_point = (code_point << 6) | (cont_byte & 0x3F)
-            idx += 1
 
-        if not is_valid_code_point(code_point) or is_overlong_encoding(byte_count, code_point):
-            return False
+if __name__ == "__main__":
+    import random
 
-    return True
+    def generate_random_integer_list(length):
+        return [random.randint(0, 255) for _ in range(length)]
 
+    # Example usage:
+    random_list = generate_random_integer_list(2)
+    print(random_list)
+    print(validUTF8(random_list))
