44a48e87cb4b91bb44d0f3a79b2cf220e79dbe43
alexUd01 <alexanderikpeama@gmail.com>
2023-08-03 20:32:55 +0100

    nqueen

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -50,6 +50,7 @@ class NQueen:
 
 # Main
 
-if len(sys.argv) != 2:
-    print("Usage: nqueens N")
-    sys.exit(1)
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
