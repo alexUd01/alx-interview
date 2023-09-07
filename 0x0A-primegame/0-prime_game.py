25926ea94ffa0ab8ebbb8871548a51fbac0f117f
alexUd01 <alexanderikpeama@gmail.com>
2023-09-07 14:52:04 +0100

    task 0

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -37,5 +37,62 @@ Result: Ben has the most wins
 """
 
 
+def is_prime(num):
+    """ A helper function that returns true if `num` is prime """
+    if num in [None, 0, 1]:
+        return False
+    if num == 2:
+        return True
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    return True
+
+
+players_score = {'Maria': 0, 'Ben': 0}
+
+
 def isWinner(x, nums):
     """ The function """
+    if x is None or nums is None or x > len(nums):
+        return None
+    curr_player = None  # Maria starts the game always
+    for i in range(x):
+        n = nums[i]
+        temp = list(range(1, n + 1))
+        for item in temp.copy():
+            if temp == [1]:
+                if curr_player == None:
+                    players_score['Maria'] += 1
+                    #>print("{} won this round!\n".format('Maria'))
+                else:
+                    curr_player = \
+                        'Maria' if curr_player in [None, 'Ben'] else 'Ben'
+                    players_score[curr_player] += 1
+                    #>print("{} won this round!\n".format(curr_player))
+                break
+
+            if is_prime(item):
+                curr_player = 'Maria' if curr_player in [None, 'Ben'] else 'Ben'
+                # remove multiples of `item` from the list
+                temp = list(filter(lambda x: (x % item) != 0, temp))
+                #>print("{} removed {}... ==== {}".format(curr_player, item, temp))
+                if temp == [1]:
+                    players_score[curr_player] += 1
+                    #>print("{} won this round!\n".format(curr_player))
+                    break
+                continue
+    #>print("Maria = {}, Ben = {}".format(players_score['Maria'], players_score['Ben']))
+    if players_score['Maria'] > players_score['Ben']:
+        return 'Maria'
+    else:
+        return 'Ben'
+
+# 1. Organize your thoughts (Philipians 4:8)
+# 2. Living a respectful life. Love and fear of God.
+# 3. Study the word of the lrd continually
+# 4.
+# 5.
+if __name__ == "__main__":
+    for i in range(100):
+        print("{} {}".format(i, is_prime(i)))
